912c8d6862fb71ab96ad744252c735d7be19a3d1
f3kilo <f3kilo@yandex.ru>
2020-09-26 12:20:28 +0300

    Clone Tex.

--- src/tex.rs
+++ src/tex.rs
@@ -1,12 +1,25 @@
 use crate::back::TexId;
 use crate::tex_waiter::TexRemover;
+use std::sync::Arc;
 
+#[derive(Clone)]
 pub struct Tex {
+    inner: Arc<UniqueTex>,
+}
+
+impl Tex {
+    pub fn new(id: TexId, unloader: TexRemover) -> Self {
+        let inner = Arc::new(UniqueTex::new(id, unloader));
+        Self { inner }
+    }
+}
+
+struct UniqueTex {
     id: TexId,
     remover: TexRemover,
 }
 
-impl Tex {
+impl UniqueTex {
     pub fn new(id: TexId, unloader: TexRemover) -> Self {
         Self {
             id,
@@ -15,7 +28,7 @@ impl Tex {
     }
 }
 
-impl Drop for Tex {
+impl Drop for UniqueTex {
     fn drop(&mut self) {
         self.remover.remove(self.id)
     }
